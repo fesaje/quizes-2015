fc9ce39ffd90d50367e39f9b8c05f42b1294351e
fesaje <fesaje@gmail.com>
2015-06-22 11:12:59 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ router.get('/author', function(req, res) {
 });
 
 
+router.param('quizId', quizController.load);
+
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
